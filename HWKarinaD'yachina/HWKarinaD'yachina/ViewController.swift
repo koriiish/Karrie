ae042c8cf4dfba8de445898731390df2617c9e35
koriiish <karinad000@gmail.com>
2023-11-28 18:26:54 +0300

    Update even check part

--- HWKarinaD'yachina/HWKarinaD'yachina/ViewController.swift
+++ HWKarinaD'yachina/HWKarinaD'yachina/ViewController.swift
@@ -11,9 +11,51 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let first = 2.5
+        let second = 9.7
+        let third = 6.9
+        let forth = 8.2
+        
+        let firstInt = Int(first)
+        let secondInt = Int(second)
+        let thirdInt = Int(third)
+        let forthInt = Int(forth)
+        
+        let sum = firstInt + secondInt + thirdInt + forthInt
+        print(sum)
+        
+        
+        let firstDouble = first.truncatingRemainder(dividingBy: 1)
+        let secondDouble = second.truncatingRemainder(dividingBy: 1)
+        let thirdDouble = third.truncatingRemainder(dividingBy: 1)
+        let forthDouble = forth.truncatingRemainder(dividingBy: 1)
+        
+        let sumDouble = firstDouble + secondDouble + thirdDouble + forthDouble
+        print(sumDouble)
+        
+        //проверка на четные/нечетные через цикл for in
+        let numbers = [2, 9, 6, 8]
+        for number in numbers{
+            number % 2 == 0 ? print ("Число четное") : print ("Число нечетное")
+            }
+        }
+        //проверка на четное число
+
+      /*  let isNumberEven = firstInt % 2
+        if isNumberEven == 0{
+            print ("Число четное")
+        } else {
+            print ("Число нечетное")
+        }
+        
+        let isNumberEvenTwo = secondInt % 2
+        if isNumberEvenTwo == 0{
+            print ("Число четное")
+        } else {
+            print ("Число нечетное")*/
+        
+        
+        
     }
 
-
-}
 
