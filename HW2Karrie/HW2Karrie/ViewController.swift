9ff8ce837aba43d16bd8c3bcd2d34d1b1b41cc86
Karrie <karinad000@gmail.com>
2023-12-04 19:40:00 +0300

    All HW added

--- HW2Karrie/HW2Karrie/ViewController.swift
+++ HW2Karrie/HW2Karrie/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter = 0
+    let letter = ["авб", "ввш"]
+    var a: String = "uuu"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //1. Универсальные функции сложения, вычитания, умножения и деления (перегрузка функций)
@@ -68,8 +72,35 @@ class ViewController: UIViewController {
         firstA < secondA ? print("Вторая строка больше") : print ("Первая строка больше")
         */
         
+        // через замыкание сравнивает и печатает в последовательности от большего к меньшему
+        let lettersClosure = letter.sorted(by: { (s1: String, s2: String) -> Bool in
+           return s1 > s2
+        })
+        print (lettersClosure)
+        
+     
         //4. Циклический вызов функций - поломать приложение
-
+        
+        // закомментила, чтобы остальное проверять)
+/*      func breakAnApp () {
+            var summa = 2
+            while summa > 1{
+                summa += 1
+                print(summa)
+            }
+        }
+        breakAnApp()
+ */
+        // тоже самое только через repeat - while
+ /*       func breakTheApp() {
+            var count = 1
+            repeat {
+                count += 1
+                print (count)
+            } while count > 0
+        }
+        breakTheApp()
+  */
         //5. Функция возведения в степень с дефолтным параметром
         func numberInPower (number: Decimal, power: Int){
             print(pow(number,power))
@@ -84,19 +115,46 @@ class ViewController: UIViewController {
         substractClosure(10, 8)
 
         //*** Замыкание внутри которой при вызове происходит увеличение счётчика на +1
-        /*var numberPlusOne: Int = 0
-        let numberPlusOneClosure = {(numberPlusOne: Int) in
-            numberPlusOne += 1
-         */
+        // обычная функция
+       /* func counterPlusOne() {
+                counter += 1
+                print(counter)
+            }
+        counterPlusOne()
+        counterPlusOne()
+        counterPlusOne()
+        */
         
+        // self предложил добавить Xcode (по идее мы явно ссылаемся?)
+        let counterPlusOneClosure = { [self](amount: Int) in
+            self.counter += amount
+            print (counter)
+        }
+        counterPlusOneClosure(1)
+        counterPlusOneClosure(1)
         
         //***Замыкание в котором ты проверяешь полил ли ты цветок, если нет то напоминание полить цветок
         
+        let toWaterFlowersClosure = { [self](water: String) in
+            if water == a {
+                return ("Attaboy")
+            } else {
+                return ("You should water flower")
+            }
+        }
+       print (toWaterFlowersClosure("pour"))
         
         //*** Выполнить любое замыкание внутри замыкания по очереди/ замыкание внутри любой функции
-
-        
-
+        func action() -> (()->Int){
+            var val = 0
+            return {
+                val = val+1
+                return val
+            }
+        }
+        let inc = action()
+        print(inc())
+        print(inc())
         
         }
     
